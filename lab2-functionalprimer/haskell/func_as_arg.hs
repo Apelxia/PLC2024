60037bd3b01f77b1bdded8ce4b3b80d4d9d31927
Maahi Ahmed <maahi63004@gmail.com>
2024-01-29 15:30:11 +0000

    Functions Task Haskell Complete

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,22 +1,10 @@
 --create inpFunc
-inpFunc = [a..b] where 
-    a = 1 --this will be replaced with user input
-    b = 5 --this will be replaced with user input
+inpFunc a b = [a..b] 
 
 --Define applicatorFunc
-applicatorFunc :: [Int] -> Char -> Double
-applicatorFunc inpFunc s =
-  if s == 's'
-    then fromIntegral (sum inpFunc)
-    else fromIntegral (sum inpFunc) / fromIntegral (length inpFunc)
-
+applicatorFunc inpFunc a b s | s == 's' =  sum (inpFunc a b)
+                             | otherwise = (sum (inpFunc a b))/(b-a+1)
 
 main = do
-  putStrLn "Enter the start and end values for the list (a b):"
-  input <- getLine
-  let [start, end] = map read (words input) :: [Int]
-      inpFunc = [start..end]
-      result = applicatorFunc inpFunc 'a'
-  putStrLn ("sum/average = " ++ show result)
-
- 
+    let result = applicatorFunc inpFunc 2 10 's' --Call applicatorFunc with inpFunc and 'a' as args
+    putStrLn("sum = " ++ show(result))
