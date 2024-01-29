4ad65b5f00f50d5f40ebe79e078505984197d386
Maahi Ahmed <maahi63004@gmail.com>
2024-01-29 13:32:00 +0000

    Task 1 and 2 Completed

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,9 @@
 main = do
+    let oneplusone :: String
+        oneplusone = "1 + 1 = 2"
+
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    
+    print (oneplusone)
